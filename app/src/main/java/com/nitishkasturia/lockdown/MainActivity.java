76e12f67158a208e3c77c3dc427530efcb28ed84
Nitish Kasturia <nitishkasturia@gmail.com>
2014-08-02 01:57:25 -0400

    HelpActivity added Action Bar updated

--- app/src/main/java/com/nitishkasturia/lockdown/MainActivity.java
+++ app/src/main/java/com/nitishkasturia/lockdown/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nitishkasturia.lockdown;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,23 +14,25 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if(id == R.id.action_new_profile){
+            //Create New Profile
+        }else if(id == R.id.action_change_PIN){
+            //Change PIN
+        }else if(id == R.id.action_global_settings){
+            //Global Settings
+        }else if (id == R.id.action_help) {
+            startActivity(new Intent(this, HelpActivity.class));
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
